0258c7e8e36fbac5767c2515a317455c1e7e820d
Douglas Sweetser <sweetser@alum.mit.edu>
2014-10-10 19:04:59 -0400

    Updated.

--- Q8/cubes_gamelogic.py
+++ Q8/cubes_gamelogic.py
@@ -1,5 +1,6 @@
 import bge
 from bge import logic
+import q8
 
 def main(cont):
     own = cont.owner
@@ -10,5 +11,7 @@ def main(cont):
     objects["cube_101"].visible = True
     objects["cube_102"].visible = False
 
-    objects["cube_101"].position.x += 0.1
-    objects["cube_101"].position.x -= 0.2
+    d1 = [0.001, 0.005, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008]
+    qd1 = q8.Q8(random = True)
+    qd1.constant_velocity(objects["cube_101"], d1)
+    qd1.constant_velocity(objects["cube_102"], qd1.q8)
