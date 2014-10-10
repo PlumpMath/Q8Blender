167af2da3525f8d82b35b0e20305d289d8c50dd7
Douglas Sweetser <sweetser@alum.mit.edu>
2014-10-10 18:01:23 -0400

    Can move objects.

--- Q8/cubes_gamelogic.py
+++ Q8/cubes_gamelogic.py
@@ -6,8 +6,9 @@ def main(cont):
 
     scene = logic.getCurrentScene()
     objects = scene.objects
-    
     objects["cube_100"].visible = False
     objects["cube_101"].visible = True
     objects["cube_102"].visible = False
 
+    objects["cube_101"].position.x += 0.1
+    objects["cube_101"].position.x -= 0.2
