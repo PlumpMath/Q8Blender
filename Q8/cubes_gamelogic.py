88da6e0127171521b3853f6adb864ee0a91a8218
Douglas Sweetser <sweetser@alum.mit.edu>
2014-10-10 23:36:52 -0400

    Has a clock.

--- Q8/cubes_gamelogic.py
+++ Q8/cubes_gamelogic.py
@@ -9,9 +9,20 @@ def main(cont):
     objects = scene.objects
     objects["cube_100"].visible = False
     objects["cube_101"].visible = True
-    objects["cube_102"].visible = False
+    objects["cube_102"].visible = True
 
     d1 = [0.001, 0.005, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008]
     qd1 = q8.Q8(random = True)
     qd1.constant_velocity(objects["cube_101"], d1)
-    qd1.constant_velocity(objects["cube_102"], qd1.q8)
+    qd1.constant_velocity(objects["cube_102"], d1)
+
+    # A cyclic timer
+
+    if not 'timer' in own:
+        own['timer'] = 0
+
+    repeat_after = 600
+    own['timer'] += 1
+    own['timer'] %= repeat_after
+
+    print(own['timer'])
